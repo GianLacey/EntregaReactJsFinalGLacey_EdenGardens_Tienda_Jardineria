22124fe6de58c5cb5193e3adacecb1633a420b38
Gian Tomas Lacey <glacey@ssn.gob.ar>
2023-12-05 15:58:06 -0300

    siguiendo la clase Promises

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 
 import './App.css'
+import CountComponent from './components/CountComponent/CountComponent'
 
 import { ItemListContainer } from './components/ItemsListContainer/ItemListContainer'
 import MainLayout from './layouts/MainLayout'
 
 function App() {
 
+  const promesa = new Promise((resolve, reject) => {
+    
+    const flag = false;
+
+    if(flag) {
+      resolve('Promesa correcta');
+    } else {
+      reject('Promesa negativa');
+    }
+    
+  });
+
+  promesa.then(res => console.log(res)).catch(e => console.error(e));
+
   return (
     <MainLayout>
-      <ItemListContainer greeting='Eden Gardens'/>
+      <ItemListContainer greeting='Eden Gardens' />
+      <CountComponent></CountComponent>
     </MainLayout>
   )
 }
 
 export default App
+ 
