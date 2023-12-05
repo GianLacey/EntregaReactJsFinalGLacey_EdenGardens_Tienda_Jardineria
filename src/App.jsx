d379e5efcbfcc90eab1c583a56ce13c76ccbd57c
Gian Tomas Lacey <glacey@ssn.gob.ar>
2023-12-05 16:59:23 -0300

    probando

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,25 @@ function App() {
     
   });
 
+  const array = [1, 2, 3, 4, 5, 6, 7]
+
   promesa.then(res => console.log(res)).catch(e => console.error(e));
 
   return (
     <MainLayout>
       <ItemListContainer greeting='Eden Gardens' />
       <CountComponent></CountComponent>
+      <ul className='ml-5 p-5 text-lime-600 bg-gray-900 inline-flex'>
+        {
+          array.map(item =>{
+            return (
+              <li>
+                {item}
+              </li>
+            )
+          })
+        }
+      </ul>
     </MainLayout>
   )
 }
