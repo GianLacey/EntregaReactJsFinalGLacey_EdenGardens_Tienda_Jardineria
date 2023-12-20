90c576e84fbf9ddf88d9dcc2df6fd975fe5f1306
Gian Tomas Lacey <glacey@ssn.gob.ar>
2023-12-20 13:36:48 -0300

    newsletter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,37 +2,37 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-        <footer className="mt-52 text-center relative font-">
-            <div className="absolute top-[-200px] w-full z-10">
-                <div className="absolute right-20 rounded h-52 p-10 w-5/6 text-center text-white bg-green-600">
-                    <h2 className="mb-8">Sumate al Newsletter</h2>
+        <footer className="mt-52 text-center relative font-monserrat">
+            <div className="absolute top-[-210px] w-full z-10">
+                <div className="absolute right-20 rounded-2xl h-52 p-10 w-5/6 text-center text-white bg-green-600">
+                    <h2 className="mb-8 text-2xl">Sumate al Newsletter</h2>
                     <div className="flex justify-center items-center">
-                        <input type="text" placeholder="Nombre" className="bg-white border-none mr-2 px-3 py-4 h-4" />
-                        <input type="email" placeholder="Correo electrónico" className="bg-white mr-2 px-3 py-4 border-none h-4" />
-                        <button type="submit" className="flex px-3 py-4 w-52 items-center justify-center h-4 border border-gray-500 text-gray-700 rounded rounded-3 bg-transparent">
+                        <input type="text" placeholder="Nombre" className="w-44 bg-white border-none mr-2 px-3 py-4 h-4 text-xxs" />
+                        <input type="email" placeholder="Correo electrónico" className="w-44 bg-white mr-2 px-3 py-4 border-none h-4 text-xxs" />
+                        <button type="submit" className="flex px-3 py-4 w-44 items-center justify-center h-4 border border-gray-500 text-gray-700 rounded rounded-3 bg-transparent text-xs">
                             SUSCRIBIRSE
                         </button>
                     </div>
                 </div>
             </div>
 
-            <section className="p-16 flex text-white h-52 bg-gray-700 justify-between relative z-1">
-                <div className="">
-                    <h5>San Isidro</h5>
+            <section className="pt-14 px-10 flex text-xxs text-white h-52 bg-gray-700 justify-between relative z-1 leading-5">
+                <div>
+                    <h5 className='font-semibold mb-2'>San Isidro</h5>
                     <p>Av. Andrés Rolón 368 <br />
                         (011) 4723.3505 <br />
                         Horarios de atención: <br />
                         Lunes a Viernes: 8.30 a 13 | 14 a 19 hs <br />
                         Sábados: 9 a 13 hs</p>
                 </div>
-                <div className="">
-                    <h5>Contacto</h5>
+                <div >
+                    <h5 className='font-semibold mb-2'>Contacto</h5>
                     <p>Envianos tu consulta <strong>aquí</strong></p><br />
-                    <h5><a href="#">Nosotros</a></h5>
+                    <h5 className='font-semibold mb-2'><a href="#">Nosotros</a></h5>
                     <p><strong>Conoce más sobre Eden Garden</strong></p>
                 </div>
-                <div className="">
-                    <h5>Los Cardales</h5>
+                <div >
+                    <h5 className='font-semibold mb-2'>Los Cardales</h5>
                     <p>Estrada 44 <br />
                         (0230) 431.9102 · 115.228.8187 <br />
                         Horarios de atención: <br />
@@ -41,23 +41,22 @@ export const Footer = () => {
                 </div>
             </section>
 
-            <section className="">
-                <h3 className="text-green-600 text-16 pt-8 ">Arrepentimiento de compra</h3>
+            <section className="bg-slate-100 py-6 text-xs mt-10">
+                <h3 className="text-green-600 text-16">Arrepentimiento de compra</h3>
                 <p>Iniciá tu gestión <strong><a href="#" className="text-green-600">aquí</a></strong></p>
             </section>
 
-            <section className="bg-white p-0 40 flex items-center justify-between">
+            <section className="bg-white p-6 flex items-center justify-between">
                 <div className="flex">
-                    <img src="https://www.svgrepo.com/show/354518/visa.svg" alt="visacard" className="m-5  w-10" />
-                    <img src="https://www.svgrepo.com/show/508701/mastercard-full.svg" alt="mastercard" className="m-5  w-10 " />
-                    {/* Repite este bloque según sea necesario para cada tarjeta */}
+                    <img src="https://www.svgrepo.com/show/354518/visa.svg" alt="visacard" className="w-8 mr-2" />
+                    <img src="https://www.svgrepo.com/show/508701/mastercard-full.svg" alt="mastercard" className="w-8 " />
                 </div>
                 <div className="flex items-center">
-                    <a href="#"><img src="https://www.svgrepo.com/show/452197/facebook.svg" alt="facebook" className="m-10  w-10 " /></a>
-                    <a href="#"><img src="https://www.svgrepo.com/show/452231/instagram.svg" alt="instagram" className="m-10  w-10 " /></a>
+                    <a href="#"><img src="https://www.svgrepo.com/show/452197/facebook.svg" alt="facebook" className="w-8" /></a>
+                    <a href="#"><img src="https://www.svgrepo.com/show/452231/instagram.svg" alt="instagram" className="w-8" /></a>
                 </div>
-                <div className="dataFiscal">
-                    <a href="#"><img src="./assest/icons/tarjetas/afip-data-fiscal.png" alt="" className="ml-150  w-40 " /></a>
+                <div className="flex">
+                    <a href="#"><img src="../src/assets/icons/tarjetas/afip-data-fiscal.png" alt="data-fiscal" className="mx-5 w-8" /></a>
                 </div>
             </section>
         </footer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     extend: {},
     fontSize: {
       xxs: '0.6rem',
+      xs: '11.7px',
       xsmed: '14px',
       sm: '0.8rem',
       base: '1rem',
@@ -16,7 +17,8 @@ export default {
     },
     fontFamily: {
       'segoeui': ['Tahoma, Verdana, sans-serif'],
-      'franklin': ['Franklin Gothic Medium, Arial Narrow, Arial, sans-serif, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue, sans-serif']
+      'franklin': ['Franklin Gothic Medium, Arial Narrow, Arial, sans-serif, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue, sans-serif'],
+      'monserrat': ['Montserrat, sans-serif']
     },
     backgroundImage: {
       'main-img': "url(https://media.admagazine.com/photos/618a64f9b67f79aa891ed98e/master/w_1600%2Cc_limit/71307.jpg)",
